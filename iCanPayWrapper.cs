abb8b3956af2cb7b50171ab7d0381889b9afbf3b
Pranav Zadafiya <pzadafiya@gmail.com>
2018-12-28 17:44:39 +0530

    Update iCanPayWrapper.cs

--- iCanPayWrapper.cs
+++ iCanPayWrapper.cs
@@ -12,7 +12,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
 
-namespace NLFSG.BLL.Helper
+namespace BLL.Helper
 {
     public class iCanPayWrapper
     {
